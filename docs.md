baf15fddaa4a614d9a5fd0c94d09766915b99593
ramonespier <ramon.coelhomelo@outlook.com>
2025-01-21 11:15:39 -0300

    portifolio

---
+++ docs.md
@@ -0,0 +1,18 @@
+# Documentação da página de portifólio
+
+## Página home
+ - Apresentação da página
+ - Projetos
+
+## Página portifólio
+ - Apresentação da pessoal
+ - Projetos
+ - Hard skills
+ - Contato
+
+#### Header
+ - Breve apresentação da página
+
+#### Footer
+ - Redes sociais
+ - Contato
---
+++ php/projetos.php
@@ -0,0 +1,14 @@
+<?php
+
+$mustang = [
+    "nome" => "Ford | Mustang",
+    "link" => "xgh-projeto-final.vercel.app",
+    "foto" => "../img/mustang.png"
+];
+
+$focus = [
+    "nome" => "FocusHub",
+    "link" => "https://github.com/ramonespier/venda-de-cursos",
+    "foto" => "../img/focus.png"
+];
+?>
