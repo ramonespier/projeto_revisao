551bc03783e368368d9672acb00e77f9886bd222
ramonespier <ramon.coelhomelo@outlook.com>
2025-01-29 11:13:04 -0300

    salva contato

--- src/php/contato.php
+++ src/php/contato.php
@@ -1,14 +1,11 @@
 <?php
     $nome = $_POST['nome'];
     $email = $_POST['email'];
-    $assunto = $_POST['assunto'];
     $descricao = $_POST['descricao'];
 
     $handle = fopen("contato.txt", "a+");
-    fwrite($handle, $nome . " - " . $email . " - " . $assunto . " - " . $descricao . "\n");
+    fwrite($handle, $nome . ": " . $email . "\n" . $nome . ": " . $descricao . "\n");
     fclose($handle);
 
     header("location: sobre.php")
-
-
 ?>
